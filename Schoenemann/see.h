578b28a37d4ac46b190dab7620272a078a31980d
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-09-18 02:23:32 +0200

    Bench 801717

--- Schoenemann/see.h
+++ Schoenemann/see.h
@@ -2,7 +2,7 @@
 
 using namespace chess;
 
-const int SEE_PIECE_VALUES[7] = {100, 300, 300, 500, 900, 0, 0};
+const int SEE_PIECE_VALUES[7] = {126, 781, 825, 1276, 2538, 0, 0};
 
 int getPieceValue(const Board &board, Move &move);
 bool see(const Board &board, Move &move, int cutoff);
