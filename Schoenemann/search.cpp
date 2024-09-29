77003e23826e04fa48aff63075d24549d5397df1
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-09-29 10:44:28 +0200

    Bench 6629656

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -234,7 +234,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board& board)
 
             if (lmrDepth <= 7 && !board.inCheck() && alpha < (infinity - 256) && alpha > staticEval + 160 + std::max(lmrDepth, 0) * 157)
             {
-                break;
+                continue;
             } 
         }
         
