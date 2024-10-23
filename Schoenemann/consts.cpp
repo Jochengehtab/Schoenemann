3d0de861808e7426324aa7d8fa2ac41deca84b58
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-10-23 23:52:16 +0200

    .

--- Schoenemann/consts.cpp
+++
@@ -1,7 +0,0 @@
-#include "consts.h"
-
-DEFINE_PARAM_S(piecePawn, 100, 10);
-DEFINE_PARAM_S(pieceKnigh, 302, 20);
-DEFINE_PARAM_S(pieceBishp, 320, 20);
-DEFINE_PARAM_S(pieceRook, 500, 50);
-DEFINE_PARAM_S(pieceQuuen, 900, 90);
