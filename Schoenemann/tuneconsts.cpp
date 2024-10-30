ea2849df80931e48c4ad334e14681e6b5f5eacaf
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-10-30 11:26:57 +0100

    Bench 7190597

--- Schoenemann/tuneconsts.cpp
+++ Schoenemann/tuneconsts.cpp
@@ -1,18 +1,18 @@
 #include "tuneconsts.h"
 
-DEFINE_PARAM_S(seePawn, 100, 10);
-DEFINE_PARAM_S(seeKnight, 300, 30);
-DEFINE_PARAM_S(seeBishop, 300, 30);
-DEFINE_PARAM_S(seeRook, 500, 50);
-DEFINE_PARAM_S(seeQueen, 900, 90);
+DEFINE_PARAM_S(seePawn, 107, 10);
+DEFINE_PARAM_S(seeKnight, 337, 30);
+DEFINE_PARAM_S(seeBishop, 338, 30);
+DEFINE_PARAM_S(seeRook, 464, 50);
+DEFINE_PARAM_S(seeQueen, 864, 90);
 
 int SEE_PIECE_VALUES[7] = {seePawn, seeKnight, seeBishop, seeRook, seeQueen, 0, 0};
 
-DEFINE_PARAM_S(piecePawn, 100, 10);
-DEFINE_PARAM_S(pieceKnigh, 302, 20);
-DEFINE_PARAM_S(pieceBishp, 320, 20);
-DEFINE_PARAM_S(pieceRook, 500, 50);
-DEFINE_PARAM_S(pieceQuuen, 900, 90);
+DEFINE_PARAM_S(piecePawn, 105, 10);
+DEFINE_PARAM_S(pieceKnigh, 287, 20);
+DEFINE_PARAM_S(pieceBishp, 300, 20);
+DEFINE_PARAM_S(pieceRook, 438, 50);
+DEFINE_PARAM_S(pieceQuuen, 859, 90);
 
 
 int PIECE_VALUES[7] = {piecePawn, pieceKnigh, pieceBishp, pieceRook, pieceQuuen, 15000, 0};
