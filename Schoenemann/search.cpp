d6c7c8282268af448613a59ef8c264c17a5cd77f
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-11-03 03:03:03 +0100

    Bench 1491695

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -457,7 +457,7 @@ int Search::qs(int alpha, int beta, Board& board, int ply)
 
         if (move.to().back_rank(move.to(), board.sideToMove()))
         {
-            if (standPat < alpha - 1500) 
+            if (standPat + 1400 <= alpha) 
             {
                 return standPat;
             }
