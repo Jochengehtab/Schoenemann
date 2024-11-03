c4f7d8165e48dd7dd57c76f00c503d0952b6e2ab
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-11-03 03:06:51 +0100

    Revert "Bench 1491695"
    This reverts commit d6c7c8282268af448613a59ef8c264c17a5cd77f.

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -457,7 +457,7 @@ int Search::qs(int alpha, int beta, Board& board, int ply)
 
         if (move.to().back_rank(move.to(), board.sideToMove()))
         {
-            if (standPat + 1400 <= alpha) 
+            if (standPat < alpha - 1500) 
             {
                 return standPat;
             }
