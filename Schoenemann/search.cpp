350998cc69485b5758f0fa6cd715e54a6bdfa558
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-12-15 11:19:57 +0100

    Bench 1224109

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -438,7 +438,7 @@ int Search::qs(int alpha, int beta, Board &board, int ply)
         }
     }
 
-    if (!inCheck)
+    if (!inCheck && zobristKey == (zobristKey & 0xFFFF))
     {
         standPat = hashedScore;
     }
