f6dd6ef97a9aafb051d0c14ea067264465f2e0fb
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-12-23 00:34:16 +0100

    Bench 740817

--- Schoenemann/moveorder.cpp
+++ Schoenemann/moveorder.cpp
@@ -38,7 +38,7 @@ void orderMoves(Movelist& moveList, Hash* entry, Board& board, int scores[], Mov
 		}
 		else 
 		{
-			scores[i] += searcher.quietHistory[board.sideToMove()][board.at(move.from()).type()][move.to().index()] + searcher.stack[ply].continuationHistory[searcher.stack[ply - 1].previousMovedPiece][searcher.stack[ply - 1].previousMove.to().index()][board.at(move.to()).type()][move.to().index()];
+			scores[i] += searcher.quietHistory[board.sideToMove()][board.at(move.from()).type()][move.to().index()] + searcher.stack[ply - 1].continuationHistory[searcher.stack[ply - 1].previousMovedPiece][searcher.stack[ply - 1].previousMove.to().index()][board.at(move.to()).type()][move.to().index()];
 		}
 	}
 	
