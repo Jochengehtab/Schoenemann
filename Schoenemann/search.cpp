fa26d9cb00c0c2e60524142a8904f75cdb8bcfe8
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-12-26 01:11:54 +0100

    Bench 817709

--- Schoenemann/search.cpp
+++ Schoenemann/search.cpp
@@ -324,7 +324,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
             continue;
         }
 
-        if (!pvNode && beta >= stack[ply + 1].previousCapture + staticEval) 
+        if (!pvNode && beta >= stack[ply - 1].previousCapture + staticEval) 
         {
             staticEval += board.at(move.to()).type();
         }
