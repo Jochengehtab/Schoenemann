cc24a04554fe8d557859bafc51b8eb5dbfe7e178
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-12-29 22:03:56 +0100

    Bench 5572052

--- src/tuneconsts.cpp
+++ src/tuneconsts.cpp
@@ -14,5 +14,4 @@ DEFINE_PARAM_S(pieceBishop, 220, 20);
 DEFINE_PARAM_S(pieceRook, 524, 50);
 DEFINE_PARAM_S(pieceQueen, 603, 90);
 
-
 int PIECE_VALUES[7] = {piecePawn, pieceKnight, pieceBishop, pieceRook, pieceQueen, 15000, 0};
