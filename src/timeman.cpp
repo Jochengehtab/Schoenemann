1c24c62b1aced8e0d60bc4dda86b0b1a5a79c33b
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-12-30 21:59:05 +0100

    Bench 4324435

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -2,18 +2,12 @@
 
 void getTimeForMove()
 {
-
-	if (searcher.timeLeft < 0)
-	{
-		searcher.timeLeft = 1000;
-	}
-
 	searcher.timeLeft -= searcher.timeLeft / 2;
 	searcher.hardLimit = searcher.softLimit = searcher.timeLeft;
 
-	int baseTime = (int)(searcher.timeLeft * 0.054 + searcher.increment * 0.85);
-	int maxTime = (int)(searcher.timeLeft * 0.76);
+	int baseTime = (int) (searcher.timeLeft * 0.054 + searcher.increment * 0.85);
+	int maxTime = (int) (searcher.timeLeft * 0.76);
 
-	searcher.hardLimit = std::min(maxTime, (int)(baseTime * 3.04));
-	searcher.softLimit = std::min(maxTime, (int)(baseTime * 0.76));
+	searcher.hardLimit = std::min(maxTime, (int) (baseTime * 3.04));
+	searcher.softLimit = std::min(maxTime, (int) (baseTime * 0.76));
 }
