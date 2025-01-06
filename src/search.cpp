fddc9a9ef3347793d7b6890e39a3f98b18635b9c
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-01-06 09:29:15 +0100

    Bench 3439826

--- src/search.cpp
+++ src/search.cpp
@@ -361,7 +361,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
         // Late move prunning
         if (!pvNode && isQuiet && bestScore > -infinity && moveCounter > (3 + depth * depth * 3))
         {
-            continue;
+            break;
         }
 
         // Update the the piece and the move for continuationHistory
