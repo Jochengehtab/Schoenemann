cd09bfc52ddd2b7ccce6ca458aab26449b552f52
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-01-09 18:08:09 +0100

    Revert "Bench 4310512"
    This reverts commit 28287f8180f80fa1022300328cd0ff612901d1ad.

--- src/search.cpp
+++ src/search.cpp
@@ -757,10 +757,7 @@ void Search::iterativeDeepening(Board &board, bool isInfinite)
     {
         scoreData = i >= aspEntryDepth ? aspiration(i, scoreData, board) : pvs(-infinity, infinity, i, 0, board, false);
 
-        if (rootBestMove != Move::NULL_MOVE) 
-        {
-            bestMoveThisIteration = rootBestMove;
-        }
+        bestMoveThisIteration = rootBestMove;
         hasOneLegalMove = bestMoveThisIteration != Move::NULL_MOVE && bestMoveThisIteration != Move::NO_MOVE;
 
         // This is ugly but whitout it i get time losses etc
