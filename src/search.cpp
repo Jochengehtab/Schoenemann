b3af51b5179a3e4c0bb54053cc004012661932b8
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-01-11 08:49:40 +0100

    Bench 4434544

--- src/search.cpp
+++ src/search.cpp
@@ -531,7 +531,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
         transpositionTabel.storeEvaluation(zobristKey, depth, finalType, transpositionTabel.scoreToTT(bestScore, ply), bestMoveInPVS, staticEval);
     }
 
-    if (!inCheck && (bestMoveInPVS == Move::NULL_MOVE || !board.isCapture(bestMoveInPVS)) && (finalType == EXACT || (finalType == UPPER_BOUND && bestScore <= staticEval) || (finalType == LOWER_BOUND && bestScore > staticEval)))
+    if (!inCheck && (bestMoveInPVS != Move::NULL_MOVE || !board.isCapture(bestMoveInPVS)) && (finalType == EXACT || (finalType == UPPER_BOUND && bestScore <= staticEval) || (finalType == LOWER_BOUND && bestScore > staticEval)))
     {
         int bonus = std::clamp((int)(bestScore - staticEval) * depth * 200 / 1024, -CORRHIST_LIMIT / 4, CORRHIST_LIMIT / 4);
         updatePawnCorrectionHistory(bonus, board);
