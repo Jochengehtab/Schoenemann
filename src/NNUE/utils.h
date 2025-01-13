349c52a25bde54c4da93245eca9eaef524f729e2
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-01-13 22:40:42 +0100

    idk

--- src/NNUE/utils.h
+++ src/NNUE/utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <immintrin.h>
 
 #include "nnueconstants.h"
 
@@ -55,11 +56,34 @@ public:
         const short bucket)
     {
         int eval = 0;
+#ifdef __AVX2__
+        const __m256i vec_zero = _mm256_setzero_si256();
+        const __m256i vec_qa = _mm256_set1_epi16(QA);
+        __m256i sum = vec_zero;
+
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            const __m256i us_vec = _mm256_loadu_si256((const __m256i *)(us.data() + i));
+        const __m256i them_vec = _mm256_loadu_si256((const __m256i *)(them.data() + i));
+        const __m256i us_weights = _mm256_loadu_si256((const __m256i *)(outputWeight[bucket].data() + i));
+        const __m256i them_weights = _mm256_loadu_si256((const __m256i *)(outputWeight[bucket].data() + i + hiddenSize));
+
+        const __m256i us_clamped = _mm256_min_epi16(_mm256_max_epi16(us_vec, vec_zero), vec_qa);
+        const __m256i them_clamped = _mm256_min_epi16(_mm256_max_epi16(them_vec, vec_zero), vec_qa);
+
+        const __m256i us_results = _mm256_madd_epi16(_mm256_mullo_epi16(us_weights, us_clamped), us_clamped);
+        const __m256i them_results = _mm256_madd_epi16(_mm256_mullo_epi16(them_weights, them_clamped), them_clamped);
+
+        sum = _mm256_add_epi32(sum, us_results);
+        sum = _mm256_add_epi32(sum, them_results);
+    }
+#else
         for (std::uint16_t i = 0; i < hiddenSize; i++)
         {
-           eval += screlu(us[i]) * outputWeight[bucket][i] +
-                   screlu(them[i]) * outputWeight[bucket][i + hiddenSize];
+            eval += screlu(us[i]) * outputWeight[bucket][i] +
+                    screlu(them[i]) * outputWeight[bucket][i + hiddenSize];
         }
+#endif
         eval /= QA;
         eval += outputBias[bucket];
         eval *= scale;
