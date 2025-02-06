079425146e4e9c367cd85e7fd3fad80d1bd7eae3
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-02-06 23:38:37 +0100

    Bench 33157144

--- src/search.cpp
+++ src/search.cpp
@@ -378,7 +378,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
 
         if (!isSingularSearch && hashedMove == move && depth >= 6 && hashedDepth >= depth - 3 && (hashedType != UPPER_BOUND) && std::abs(hashedScore) < infinity && !(ply == 0))
         {
-            const int singularBeta = hashedScore - depth;
+            const int singularBeta = -2 * depth;
             const std::uint8_t singualrDepth = (depth - 1) / 2;
 
             stack[ply].exludedMove = move;
@@ -407,7 +407,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
 
         if (board.inCheck())
         {
-            extensions++;
+            extensions = 1;
         }
 
         if (moveCounter == 1)
