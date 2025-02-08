32a57b0d4a87d4615615c1e037e49b0c3578f291
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-02-08 16:38:17 +0100

    Bench 5438824

--- src/search.h
+++ src/search.h
@@ -63,7 +63,7 @@ public:
 	std::array<std::array<std::array<int, 64>, 6>, 2> quietHistory;
 	std::array<std::array<std::array<std::array<int, 6>, 64>, 6>, 64> continuationHistory;
 	std::array<std::array<std::uint8_t, 218>, 150> reductions;
-	std::array<SearchStack, 150> stack{};
+	std::array<SearchStack, 150> stack;
 
 	int pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCutNode);
 	int qs(int alpha, int beta, Board &board, int ply);
