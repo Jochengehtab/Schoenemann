e99f2729e90ba77c6937acac660d4a29b1a08eaa
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-02-16 09:59:50 +0100

    Revert "Bench 5386172"
    This reverts commit 1d73ace11f1dc04d912bd494cafa571c954c2172.

--- src/search.cpp
+++ src/search.cpp
@@ -429,7 +429,7 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
 
             else if (hashedScore >= beta)
             {
-                extensions -= 2;
+                extensions -= singularTTSub;
             }
         }
 
