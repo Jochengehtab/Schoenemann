41c9e6800146dd444554dd63c96279bd0e9ba35f
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-02-23 17:09:06 +0100

    Bench 6575031

--- src/timeman.cpp
+++ src/timeman.cpp
@@ -30,7 +30,7 @@ void getTimeForMove()
   int maxTime = (int)(searcher.timeLeft * 0.76);
 
   searcher.hardLimit = std::min(maxTime, (int)(baseTime * 3.04));
-  searcher.softLimit = std::min(maxTime, (int)((baseTime * 0.76) - bestMoveStabilityCount * 20));
+  searcher.softLimit = std::min(maxTime, (int)((baseTime * 0.76) - bestMoveStabilityCount));
 }
 
 void updateBestMoveStability(Move bestMove, Move previousBestMove)
