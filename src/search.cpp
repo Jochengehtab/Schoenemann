cd70561079d2c29182b7bf763dff77bff54e0ce6
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-03-08 11:38:57 +0100

    Bench 7097690

--- src/search.cpp
+++ src/search.cpp
@@ -332,49 +332,6 @@ int Search::pvs(int alpha, int beta, int depth, int ply, Board &board, bool isCu
         }
     }
 
-    // Idea by Laser
-    // If we can make a winning move and can confirm that when we do a lower depth search
-    // it causes a beta cutoff we can make that beta cutoff
-    if (!isSingularSearch && !pvNode && !inCheck && depth >= winningDepth && staticEval >= beta - winningEvalSub - winningDepthMul * depth && std::abs(beta) < infinity)
-    {
-        int probCutMargin = beta + probeCutMarginAdd;
-        int probCutCount = 0;
-
-        Movelist moveList;
-        movegen::legalmoves(moveList, board);
-
-        int scoreMoves[218] = {0};
-        // Sort the list
-        orderMoves(moveList, entry, board, scoreMoves, ply);
-
-        for (int i = 0; i < moveList.size() && probCutCount < winningCount; i++)
-        {
-            probCutCount++;
-            Move move = sortByScore(moveList, scoreMoves, i);
-
-            // We don't want to prune the hashed move
-            if (move == hashedMove)
-            {
-                continue;
-            }
-
-            // Update the the piece and the move for continuationHistory
-            stack[ply].previousMovedPiece = board.at(move.from()).type();
-            stack[ply].previousMove = move;
-
-            board.makeMove(move);
-
-            int score = -pvs(-probCutMargin, -probCutMargin + 1, depth - depth / winningDepthDiv - winningDepthSub, ply + 1, board, false);
-
-            board.unmakeMove(move);
-
-            if (score >= probCutMargin)
-            {
-                return score;
-            }
-        }
-    }
-
     if (!isSingularSearch && !pvNode && !inCheck && depth >= nmpDepth && staticEval >= beta)
     {
         board.makeNullMove();
