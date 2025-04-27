480c46f742e0856c1c348c3cdf39f754bc0dba2d
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-04-27 10:40:53 +0200

    Bench 3440872

--- src/search_fwd.h
+++ src/search_fwd.h
@@ -4,12 +4,12 @@
 #include "chess.hpp"
 using namespace chess;
 
-struct alignas(2048) SearchStack
+struct alignas(1024) SearchStack
 {
 	int staticEval;										// (4 Byte)
 	std::uint16_t pvLength;								// (2 Byte)				
 	bool inCheck;										// (1 Byte)
-	std::array<Move, 255> pvLine;						// (1020 Byte)
+	std::array<Move, 245> pvLine;						// (980 Byte)
 	Move killerMove = Move::NULL_MOVE;					// (4 Byte)
 	PieceType previousMovedPiece = PieceType::NONE;		// (1 Byte)
 	Move previousMove = Move::NULL_MOVE;				// (4 Byte)
