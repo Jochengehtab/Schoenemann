b764ef72557ed33b9ba91d7781e0fe4147a51557
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-05-05 21:42:01 +0200

    Bench 4152772

--- src/tune.cpp
+++ src/tune.cpp
@@ -68,7 +68,6 @@ std::string engineParameterToSpsaInput()
     return stream.str();
 }
 
-#ifdef DO_TUNING
 
 DEFINE_PARAM_B(seePawn, 140, 100, 180);
 DEFINE_PARAM_B(seeKnight, 287, 200, 350);
@@ -76,10 +75,6 @@ DEFINE_PARAM_B(seeBishop, 348, 280, 400);
 DEFINE_PARAM_B(seeRook, 565, 500, 700);
 DEFINE_PARAM_B(seeQueen, 1045, 840, 1240);
 DEFINE_PARAM_B(seeEmpty, 0, 0, 0);
-
-EngineParameter* SEE_PIECE_VALUES[7] = {
-    &seePawn, &seeKnight, &seeBishop, &seeRook, &seeQueen, &seeEmpty, &seeEmpty
-};
 
 DEFINE_PARAM_B(piecePawn, 73, 53, 93);
 DEFINE_PARAM_B(pieceKnight, 258, 208, 288);
@@ -89,31 +84,22 @@ DEFINE_PARAM_B(pieceQueen, 569, 530, 700);
 DEFINE_PARAM_B(pieceKing, 15000, 15000, 15000);
 DEFINE_PARAM_B(pieceEmpty, 0, 0, 0);
 
+#ifdef DO_TUNING
+
+EngineParameter* SEE_PIECE_VALUES[7] = {
+    &seePawn, &seeKnight, &seeBishop, &seeRook, &seeQueen, &seeEmpty, &seeEmpty
+};
+
 EngineParameter* PIECE_VALUES[7] = {
     &piecePawn, &pieceKnight, &pieceBishop, &pieceRook, &pieceQueen, &pieceKing, &pieceEmpty
 };
 
 #else
 
-constexpr int seePawn = 140;
-constexpr int seeKnight = 287;
-constexpr int seeBishop = 348;
-constexpr int seeRook = 565;
-constexpr int seeQueen = 1045;
-constexpr int seeEmpty = 0;
-
 int* SEE_PIECE_VALUES[7] = {
     (int*)&seePawn, (int*)&seeKnight, (int*)&seeBishop,
     (int*)&seeRook, (int*)&seeQueen, (int*)&seeEmpty, (int*)&seeEmpty
 };
-
-constexpr int piecePawn = 73;
-constexpr int pieceKnight = 258;
-constexpr int pieceBishop = 217;
-constexpr int pieceRook = 476;
-constexpr int pieceQueen = 569;
-constexpr int pieceKing = 15000;
-constexpr int pieceEmpty = 0;
 
 int* PIECE_VALUES[7] = {
     (int*)&piecePawn, (int*)&pieceKnight, (int*)&pieceBishop,
