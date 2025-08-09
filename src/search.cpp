82f0c7234b74b474e3d9d8305acfa37fe50bc4bd
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-08-09 15:43:43 +0200

    Bench 8111713

--- src/search.cpp
+++ src/search.cpp
@@ -132,20 +132,9 @@ int Search::pvs(int alpha, int beta, int depth, const int ply, Board &board, boo
 
     // Razoring
     if (!isSingularSearch && !pvNode && !board.inCheck() && depth <= 1) {
-        const int ralpha = alpha - razorAlpha - depth * razorDepthMul;
-
-        if (staticEval < ralpha) {
-            int qscore;
-            if (depth == 1 && ralpha < alpha) {
-                qscore = qs(alpha, beta, board, ply);
-                return qscore;
-            }
-
-            qscore = qs(ralpha, ralpha + 1, board, ply);
-
-            if (qscore <= ralpha) {
-                return qscore;
-            }
+        const int score = qs(alpha, beta, board, ply);
+        if (score <= alpha) {
+            return score;
         }
     }
 
